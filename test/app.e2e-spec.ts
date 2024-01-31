b02eab2092354f36bc6c56dfc976169e640b011a
Oleg Shkalikov <maska6293@yandex.ru>
2024-01-31 20:08:42 +0300

    init: start ozon report service project

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,10 @@
+import { request } from './lib';
+import { reportRoutes } from './endpoints';
+import { StatusCodes } from 'http-status-codes';
+
+describe('AppController (e2e)', () => {
+  const unauthorizedRequest = request;
+  const commonHeaders = { Accept: 'application/json' };
+
+  describe('GET', () => {});
+});
